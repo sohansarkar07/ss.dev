dcf3d650b63b03c7ab8bc052f6634a7d5880524c
LIFE IN DEBUG <sohansarkar13102025@gmail.com>
2025-12-30 16:53:04 +0530

    Update photo import path to relative path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import '../index.css'
 
 
 // 🖼️ Import Assets
-import photo from 'D:/Sohan.dev/public/photo.jpg'
+import photo from 'public/photo.jpg'
 import githubLogo from '../../public/github.png'
 import linkedinLogo from '../../public/linkedin.png'
 import gmailLogo from '../../public/gmail.png'
